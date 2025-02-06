f29e96a4ff3efdd7d1e7f37e30c71886b5af898a
root <ahmadmodallal87@gmail.com>
2025-02-06 13:08:20 +0000

    add

---
+++ sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_H
+#define SORT_H
+
+#include <stddef.h>
+
+/* Function to print the array */
+void print_array(const int *array, size_t size);
+
+/* Bubble Sort function */
+void bubble_sort(int *array, size_t size);
+
+#endif /* SORT_H */
+
